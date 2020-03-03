8abc578582b13fa78919fc363db3605c9d9a96ad
Karl Ritchie <ritchie.karl@gmail.com>
2020-03-03 14:46:29 -0500

    Fix fuzzy membership unit tests and add noisy membership test

--- tests/functions/test_fuzzy_membership.py
+++ tests/functions/test_fuzzy_membership.py
@@ -39,18 +39,42 @@ def test_fuzzy_membership_epsilon_error(valid_ensemble_a, valid_ensemble_b, u, e
         _ = fuzzy_membership(params, valid_ensemble_a, valid_ensemble_b)
 
 
-def test_fuzzy_membership_valid(valid_ensemble_a, valid_ensemble_b):
+def test_fuzzy_membership_no_noise(valid_ensemble_a, valid_ensemble_b):
 
     params = {
         'u': 0.5,
         'epsilon': 0.5,
     }
 
-    _fuzzy = fuzzy_membership(params, valid_ensemble_a, valid_ensemble_b)
+    _fuzzy = fuzzy_membership(params=params, class_p=valid_ensemble_a, class_n=valid_ensemble_b)
 
     _truth = np.asarray([[0.49621928], [0.47637051], [0.47637051]])
 
     assert np.isclose(_fuzzy.sp, _truth).all()
     assert np.isclose(_fuzzy.sn, _truth).all()
-    assert np.invert(np.nonzero(_fuzzy.noise_p)).all()
-    assert np.invert(np.nonzero(_fuzzy.noise_n)).all()
+
+    assert not np.nonzero(_fuzzy.noise_p)[0]
+    assert not np.nonzero(_fuzzy.noise_n)[0]
+
+
+def test_fuzzy_membership_noise(valid_ensemble_a, valid_ensemble_b):
+
+    params = {
+        'u': 0.5,
+        'epsilon': 0.5,
+    }
+
+    # Update ensemble "b" to have a point closer to center of "a"
+    valid_ensemble_b[0][0] = 0.8
+    valid_ensemble_b[0][1] = 0.8
+
+    _fuzzy = fuzzy_membership(params=params, class_p=valid_ensemble_a, class_n=valid_ensemble_b)
+
+    _truth_p = np.asarray([[0.49621928], [0.47637051], [0.47637051]])
+    _truth_n = np.asarray([[0.04410816], [0.46875], [0.46875]])
+
+    assert np.isclose(_fuzzy.sp, _truth_p).all()
+    assert np.isclose(_fuzzy.sn, _truth_n).all()
+
+    assert not np.nonzero(_fuzzy.noise_p)[0]
+    assert np.nonzero(_fuzzy.noise_n)[0] == [0]
