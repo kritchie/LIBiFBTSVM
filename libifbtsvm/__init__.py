ad0f1260b31dbc1be4d7d1673529b71185cafba4
Karl Ritchie <ritchie.karl@gmail.com>
2020-03-03 19:44:04 -0500

    Add skeleton for iFBTSVM model class

--- libifbtsvm/__init__.py
+++ libifbtsvm/__init__.py
@@ -1,4 +1,9 @@
 import logging
 
+from libifbtsvm.libifbtsvm import ifbtsvm as iFBTSVM
+
 # TODO Setup logger with appropriate config
 LOGGER = logging.getLogger(__name__)
+
+
+__all__ = ['iFBTSVM']
--- libifbtsvm/libifbtsvm.py
+++ libifbtsvm/libifbtsvm.py
@@ -1,2 +1,24 @@
 
-# TODO Add implementation of scikit-learn SVM interface
+
+from sklearn.svm import SVC
+
+
+class ifbtsvm(SVC):
+
+    def decision_function(self, X):
+        pass
+
+    def fit(self, X, y, sample_weight=None):
+        pass
+
+    def get_params(self, deep=True):
+        pass
+
+    def predict(self, X):
+        pass
+
+    def score(self, X, y, sample_weight=None):
+        pass
+
+    def set_params(self, **params):
+        pass
