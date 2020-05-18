5ee94f25549a24e513adf6206f788f7dd8bfa10a
kritchie <ritchie.karl@gmail.com>
2020-05-18 12:33:03 -0400

    F/increment model (#12)
    * WIP: Add DAG increment
    
    * Add temporary step in example to update model
    
    * WIP: add increment/decrement untested methods
    
    * WIP: Few fixes in incremental method
    
    * WIP: Add missing fixes in forget function for initial implementation
    
    * Remove changes in example_iris
    
    * Fix decrement function
    
    * Add decrement unit test and fix score update function
    
    * Add unit tests and few fixes for increment function
    
    * Fix filter gradients and unit tests
    
    * Fix linting

---
+++ examples/example_update_model.py
@@ -0,0 +1,51 @@
+
+import time
+
+from sklearn.datasets import load_iris as load_data
+from sklearn.utils import shuffle
+
+from libifbtsvm import iFBTSVM
+from libifbtsvm.models.ifbtsvm import Hyperparameters
+
+
+if __name__ == '__main__':
+
+    dataset = load_data()
+    dataset.data, dataset.target = shuffle(dataset.data, dataset.target)
+
+    params = Hyperparameters(
+        epsilon=0.0000001,
+        fuzzy=0.01,
+        C1=8,
+        C2=2,
+        C3=8,
+        C4=2,
+        max_evals=500,
+        phi=0.00001,
+        kernel=None,
+        repetition=1,
+    )
+
+    # Initialisation iFBTSVM
+    ifbtsvm = iFBTSVM(parameters=params, n_jobs=1)
+
+    # Training
+    before = time.monotonic()
+    ifbtsvm.fit(X=dataset.data[:int(len(dataset.data) / 2)], y=dataset.target[:int(len(dataset.data) / 2)])
+    after = time.monotonic()
+    elapsed = (after - before)
+
+    # Prediction
+    accuracy = ifbtsvm.score(X=dataset.data, y=dataset.target)
+    print(f'Accuracy iFBTSVM: {accuracy * 100.0}% Train time: {elapsed}s')
+
+    # Update Model
+    before = time.monotonic()
+    ifbtsvm.update(X=dataset.data[int(len(dataset.data) / 2):], y=dataset.target[int(len(dataset.data) / 2):],
+                   batch_size=20)
+    after = time.monotonic()
+    elapsed = (after - before)
+
+    # Prediction
+    accuracy = ifbtsvm.score(X=dataset.data, y=dataset.target)
+    print(f'Accuracy Updated iFBTSVM: {accuracy * 100.0}% Train time: {elapsed}s')
---
+++ tests/functions/test_update_model.py
@@ -0,0 +1,67 @@
+
+import pytest
+
+import numpy as np
+
+from libifbtsvm import iFBTSVM, Hyperparameters
+
+
+def test_compute_score_none():
+    params = Hyperparameters(phi=0.5)
+    svm = iFBTSVM(parameters=params)
+
+    score = None
+    c = np.zeros((5,))
+    for i in range(5):
+        c[i] = i + 1
+
+    _score = svm._compute_score(score, c)
+
+    assert np.array_equal(_score, np.asarray([[1, 2, 3, 4, 5], [1, 1, 1, 1, 1]]))
+
+
+def test_compute_score():
+    params = Hyperparameters(phi=0.5)
+    svm = iFBTSVM(parameters=params)
+
+    score = np.asarray([[1, 2, 3, 4, 5], [1, 1, 1, 1, 1]])
+    c = np.zeros((5,))
+    for i in range(5):
+        c[i] = i + 1
+
+    _score = svm._compute_score(score, c)
+
+    assert np.array_equal(_score, np.asarray([[1, 2, 3, 4, 5], [2, 2, 2, 2, 2]]))
+
+
+def test_decrement():
+
+    score = np.asarray([[0, 1, 2], [1, 1, 1]])
+    c = np.zeros((3, 1))
+    alphas = np.zeros((3, 1))
+    fuzzy = np.zeros((3, 1))
+    data = np.zeros((3, 1))
+
+    score, alphas, fuzzy, data = iFBTSVM._decrement([0], score, c, alphas, fuzzy, data)
+
+    assert np.array_equal(score, np.asarray([[1, 2], [1, 1]]))
+    assert np.array_equal(alphas, np.zeros((2, 1)))
+    assert np.array_equal(fuzzy, np.zeros((2, 1)))
+    assert np.array_equal(data, np.zeros((2, 1)))
+
+
+def test_filter_gradients():
+
+    weights = np.asarray([[-0.07136844], [-0.21096315], [0.31555559], [0.14247409], [-0.5475241]])
+    gradients = np.asarray([0.1, 0.2, 0.3, 0.4, 0.5])
+    data = np.asarray([[5.4, 3.4, 1.7, 0.2],
+                       [5.1, 3.4, 1.5, 0.2],
+                       [4.6, 3.1, 1.5, 0.2],
+                       [4.7, 3.2, 1.6, 0.2],
+                       [5.3, 3.7, 1.5, 0.2]])
+    label = np.ones((5, 1))
+
+    filtered = iFBTSVM._filter_gradients(weights=weights, gradients=gradients, data=data, label=label)
+
+    assert np.array_equal(filtered[0], np.asarray([[5.1, 3.4, 1.5, 0.2], [5.3, 3.7, 1.5, 0.2]]))
+    assert np.array_equal(filtered[1], np.asarray([[1], [1]]))
